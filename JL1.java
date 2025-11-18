e30ec78448042256b0e1ea5584aca344bb686c4b
eshakumara90-bit <eshakumara90@gmail.com>
2025-11-18 16:55:50 +0530

    Add files via upload

---
+++ JL1.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = s.nextLine();
+        System.out.println("Hello " + name);
+    }
+}
