5d257663cf1450a45bbefbeb580c0592d67a2c1d
eshakumara90-bit <eshakumara90@gmail.com>
2025-11-18 17:15:22 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double x = sc.nextDouble();
+        System.out.println(x * x);
+    }
+}
